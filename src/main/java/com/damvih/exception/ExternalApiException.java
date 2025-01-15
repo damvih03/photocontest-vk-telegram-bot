fbce34e873a48771f8846ac33d54d6cc080d8341
damvih03 <damvih03.official@gmail.com>
2025-01-15 17:30:19 +1000

    feat: add ExternalApiException

---
+++ src/main/java/com/damvih/exception/ExternalApiException.java
@@ -0,0 +1,9 @@
+package com.damvih.exception;
+
+public class ExternalApiException extends RuntimeException {
+
+    public ExternalApiException(String message) {
+        super(message);
+    }
+
+}
